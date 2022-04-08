8201c04e8bb18fe6c58bd529697960cda7884011
Chimort <chimort@mail.ru>
2022-04-08 11:44:58 +0600

    improvement things 1)added more characters that should not be used in counting 2)now everything is written to a file 3)added the time for which the program is executed

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import re
 
+from datetime import datetime
+
+start_time = datetime.now()
+
 words_amount = {}
 with open('text_for_tests.txt', 'r', encoding='utf-8') as f:
-    for word in re.sub('[\\\\n\\[\\]!?@#$.,;:\'\"]', '', str(f.readlines()).lower().strip()).split():
+    for word in re.sub('[\\\\n\\[\\]()_!?=+<>«»@0123456789#\-$.,;:\'\"]', '', str(f.readlines()).lower().strip()).split():
         if word not in words_amount:
             words_amount[f'{word}'] = 1
         else:
             words_amount[f'{word}'] = words_amount.get(f'{word}') + 1
 
-print(words_amount)
+f.close()
+
+print(f'Количество уникальных слов в тексте: {len(words_amount)}')
 
 sorted_dict = dict(sorted(words_amount.items(), key=lambda x: x[1]))
 
-for key, value in sorted_dict.items():
+for key, value in reversed(sorted_dict.items()):
     print(f'{key}: {value}')
+
+with open('words_amount.txt', 'w', encoding='utf-8') as file:
+    file.write(f'Количество уникальных слов в тексте: {len(words_amount)} \n')
+    for key, value in reversed(sorted_dict.items()):
+        file.write(f'{key}: {value} \n')
+
+print(f'Время выполнения программы: {datetime.now() - start_time}')
 
